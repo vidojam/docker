15d877d83b0b9351d503d17db074b80e25ca195e
Jose Torres <vidojam@gmail.com>
2023-05-05 12:00:08 -0400

    ready to build my docker image

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express"
+import cors from "cors"
+
+const app= express()
+app.use(cors())
+
+appp.get ("/", (req, res)=> {
+  res.send("Hello Docker!")
+})
+
+app.listen(3000, () => {
+  console.log ('Listen to http://localhost:3000')
+})
